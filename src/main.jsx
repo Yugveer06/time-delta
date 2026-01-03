c72355a9dd8e92979758330c5edcd89aa3a8fb5a
Yugveer Singh <Yugveer06@users.noreply.github.com>
2026-01-04 04:11:18 +0530

    chore: remove netlify redirect sugarcoat

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import NetlifyRedirect from "./components/NetlifyRedirect";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<NetlifyRedirect />
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
